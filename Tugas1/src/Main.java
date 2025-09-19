df92251be7e3ab6892bf98b5788ba128afe01d67
alyasekarrr <alyasekar.wm@gmail.com>
2025-09-19 23:51:58 +0700

    Add files via upload

---
+++ Tugas1/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Nama: Alyasekar Wafiqa Mustikaputri");
+        System.out.println("NRP: 5002251095");
+        System.out.println("Asal kota: Jakarta");
+        System.out.println("Fun fact: Ga suka lontong");
+        System.out.println("Hobi: Main + dengerin musik");
+
+    }
+}
